78bd6c63b05373ca7cba99cbc182a59572a19c71
Ubuntu <ubuntu@ip-172-31-7-231.ap-south-1.compute.internal>
2026-09-15 18:28:21 +0000

    Update app to listen on all interfaces

--- app.js
+++ app.js
@@ -9,6 +9,6 @@ app.get("/", (req, res) => {
     res.send("Hello CI/CD Pipeline");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
